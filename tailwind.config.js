d3b9fa2c9653a4c976f90a46313429c35340da36
senadev42 <senadevv42@gmail.com>
2024-04-27 14:20:08 +0300

    tailwind pallette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#5051f9",
+          lighter: "#6A6BF9",
+          light: "#9A9BFB",
+          darker: "#0D0EF0",
+          dark: "#0B0CBE",
+        },
+        accent: "#1e1f25",
+        surface: "#131517",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
