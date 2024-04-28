e4ddd0622cd7644fa6d377837122696015832c1e
senadev42 <senadevv42@gmail.com>
2024-04-28 16:06:33 +0300

    loading and error components

---
+++ src/components/Loading.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="fixed inset-0 flex flex-col gap-y-5 items-center justify-center text-center text-white">
+        <span class="animate-bounce bg-white h-15 w-15 mr-3">==</span>
+        <p class="animate-pulse"> Please wait a moment.</p>
+    </div>
+</template>
---
+++ src/views/Error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps({
+    errormessage: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="fixed inset-0 flex flex-col gap-y-4 items-center justify-center text-center text-white">
+        <p>Something went wrong.</p>
+        <p class="text-slate-500">More details: {{ errormessage }}</p>
+    </div>
+</template>
