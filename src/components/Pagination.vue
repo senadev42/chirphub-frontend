c1af0d694888aa5a9f1c207be6655760501fa728
senadev42 <senadevv42@gmail.com>
2024-04-28 14:02:24 +0300

    extract pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+
+import { computed, defineProps, defineEmits, defineExpose } from 'vue';
+import Next from "@assets/icons/Pagination/Next.svg";
+import Previous from "@assets/icons/Pagination/Previous.svg";
+
+
+const props = defineProps({
+    items: { type: Array, required: true },
+    itemsPerPage: { type: Number, required: true },
+    currentPage: { type: Number, required: true },
+});
+
+
+//logic
+const totalPages = computed(() =>
+    Math.ceil(props.items.length / props.itemsPerPage)
+);
+
+const maxPagesToShow = 5;
+const paginationItems = computed(() => {
+    const items = [];
+    const halfMaxPages = Math.floor(maxPagesToShow / 2);
+
+    let startPage = props.currentPage - halfMaxPages;
+    let endPage = props.currentPage + halfMaxPages;
+
+    if (startPage < 1) {
+        startPage = 1;
+        endPage = Math.min(maxPagesToShow, totalPages.value);
+    } else if (endPage > totalPages.value) {
+        endPage = totalPages.value;
+        startPage = Math.max(1, endPage - maxPagesToShow + 1);
+    }
+
+    items.push(1);
+    if (startPage > 1) items.push("...");
+    for (let i = startPage + 1; i <= endPage - 1; i++) items.push(i);
+    if (endPage < totalPages.value) items.push("...");
+    if (totalPages.value != 1) items.push(totalPages.value);
+
+    return items;
+});
+
+//handlers
+const emits = defineEmits(['update:currentPage']);
+
+const goToPreviousPage = () => {
+    if (props.currentPage > 1) {
+        emits('update:currentPage', props.currentPage - 1);
+    }
+};
+
+const goToNextPage = () => {
+    if (props.currentPage < totalPages.value) {
+        emits('update:currentPage', props.currentPage + 1);
+    }
+};
+
+const handlePageClick = (page: number | string) => {
+    if (page !== "...") emits('update:currentPage', page);
+};
+
+defineExpose({
+    goToPreviousPage,
+    goToNextPage,
+    handlePageClick,
+});
+
+</script>
+
+<template>
+    <div class="fixed bottom-0 left-0 w-full bg-accent flex flex-row justify-center py-3 pb-4">
+        <button @click="goToPreviousPage" class="h-10 w-10 cursor-pointer" :disabled="currentPage === 1">
+            <img :src="Previous" alt="Previous Page" />
+        </button>
+
+        <button v-for="page in paginationItems" :key="page" class="h-10 w-10 cursor-pointer text-white rounded-lg"
+            @click="handlePageClick(page)" :class="{ 'bg-brand': currentPage === page }">
+            {{ page }}
+        </button>
+
+        <button @click="goToNextPage" class="h-10 w-10 cursor-pointer ml-3" :disabled="currentPage === totalPages">
+            <img :src="Next" alt="Next Page" />
+        </button>
+    </div>
+</template>
--- src/views/BirdhouseListPage.vue
+++ src/views/BirdhouseListPage.vue
@@ -3,10 +3,10 @@ import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 
 import BirdHouseListCard from "@components/BirdHouseListCard.vue";
-import Next from "@assets/icons/Pagination/Next.svg";
-import Previous from "@assets/icons/Pagination/Previous.svg";
+import Pagination from "@components/Pagination.vue"
 
-//mocks (extract and/or match with server pagination later)
+//mocks 
+// [ ] (extract and/or match with server pagination later)
 import mockbirdhouses from "../MOCKS/mockbirdhouses.json";
 const mockbirdhousesref = ref(mockbirdhouses);
 
@@ -20,59 +20,18 @@ function handlecardclick(id: string) {
 //pagination
 const currentPage = ref(1);
 const itemsPerPage = ref(5);
-const totalPages = computed(() =>
-    Math.ceil(mockbirdhousesref.value.birdhouses.length / itemsPerPage.value)
-);
+
+
 const paginatedBirdhouses = computed(() => {
     const start = (currentPage.value - 1) * itemsPerPage.value;
     const end = start + itemsPerPage.value;
     return mockbirdhousesref.value.birdhouses.slice(start, end);
 });
 
-// pagination handler
-const goToPreviousPage = () => {
-    if (currentPage.value > 1) {
-        currentPage.value--;
-    }
-};
 
-const goToNextPage = () => {
-    if (currentPage.value < totalPages.value) {
-        currentPage.value++;
-    }
-};
 
-function handlePageClick(item: number | string) {
-    if (item !== "...") {
-        currentPage.value = +item;
-    }
-}
 
-const maxPagesToShow = 5;
-const paginationItems = computed(() => {
-    const items = [];
-    const halfMaxPages = Math.floor(maxPagesToShow / 2);
 
-    // Calculate start and end page numbers
-    let startPage = currentPage.value - halfMaxPages;
-    let endPage = currentPage.value + halfMaxPages;
-
-    if (startPage < 1) {
-        startPage = 1;
-        endPage = Math.min(maxPagesToShow, totalPages.value);
-    } else if (endPage > totalPages.value) {
-        endPage = totalPages.value;
-        startPage = Math.max(1, endPage - maxPagesToShow + 1);
-    }
-
-    items.push(1);
-    if (startPage > 1) items.push("...");
-    for (let i = startPage + 1; i <= endPage - 1; i++) items.push(i);
-    if (endPage < totalPages.value) items.push("...");
-    if (totalPages.value != 1) items.push(totalPages.value);
-
-    return items;
-});
 </script>
 
 <template>
@@ -83,18 +42,7 @@ const paginationItems = computed(() => {
     </div>
 
     <!-- pagination -->
-    <div class="fixed bottom-0 left-0 w-full bg-accent flex flex-row justify-center py-3 pb-4">
-        <button @click="goToPreviousPage" class="h-10 w-10 cursor-pointer" :disabled="currentPage === 1">
-            <img :src="Previous" alt="GPS Coordinates of Birdhouse" />
-        </button>
+    <Pagination :items="mockbirdhouses.birdhouses" :items-per-page="itemsPerPage" :current-page="currentPage"
+        @update:currentPage="currentPage = $event" />
 
-        <button v-for="page in paginationItems" :key="page" class="h-10 w-10 cursor-pointer text-white rounded-lg"
-            @click="handlePageClick(page)" :class="{ 'bg-brand': currentPage === page }">
-            {{ page }}
-        </button>
-
-        <button @click="goToNextPage" class="h-10 w-10 cursor-pointer ml-3" :disabled="currentPage === totalPages">
-            <img :src="Next" alt="GPS Coordinates of Birdhouse" />
-        </button>
-    </div>
 </template>
